21313a5b668ef14c59e766faeb9a3328ad5cc713
Mariamma Antony <Mariamma.Antony@aexp.com>
2017-10-02 20:01:56 +0530

    delete contact feature

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -6,4 +6,13 @@ describe('Address Book', function(){
 		
 		expect(addressBook.getContact(0)).toBe(thisContact);	
 	});
+
+	it('should be able to delete a contact', function(){
+		var addressBook = new AddressBook(),
+			thisContact = new Contact();
+		addressBook.addContact(thisContact);
+		addressBook.deleteContact(0);
+		
+		expect(addressBook.getContact(0)).not.toBeDefined();
+	});
 });
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -10,6 +10,10 @@ AddressBook.prototype.getContact = function(index){
 	return this.contacts[index];
 }
 
+AddressBook.prototype.deleteContact = function(index){
+	this.contacts.splice(index,1);
+}
+
 
 /*
 var contactList = [];
