d1562c8604a8bbbc610f6247a2c84ec77aadbddc
Mariamma Antony <Mariamma.Antony@aexp.com>
2017-10-13 18:58:06 +0530

    async tests

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -18,3 +18,24 @@ describe('Address Book', function(){
 		expect(addressBook.getContact(0)).not.toBeDefined();
 	});
 });
+
+describe('Async Address Book', function(){
+	var addressBook = new AddressBook();
+
+	//call done function within the callback of getInitialContacts. 
+	// This signals to the framework that our asynchronous function is
+	// done doing what we need it to do and we can continue testing
+	beforeEach(function(done){
+		addressBook.getInitialContacts(function(){
+			done();
+		});
+	});
+
+// To signal framework, which of the  tests rely on async execution, use done
+// We passed done to our function within our spec. We then called done after 
+// our tests
+	it('should grab initial contacts', function(done){
+		expect(addressBook.initialComplete).toBe(true);
+		done();
+	});
+});
